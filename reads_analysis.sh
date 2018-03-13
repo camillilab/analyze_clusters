3fe1eadc3dfb3abaffcdbbb2f8eea54d52da77cb
camillilab <37123897+camillilab@users.noreply.github.com>
2018-03-12 21:27:15 -0400

    Add files via upload

---
+++ reads_analysis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#### command lines used for same-orientation-reads (SOR) and 5-prime soft clipped reads (sCLIP) extraction from .fastq files following bwa alignement ####
+
+# Alignment of paired-end .fastq files on the reference genome using bwa #
+# Final alignment is further processed based on the mapping quality (MAPQ) #
+
+bwa index -p INDEX reference.fasta
+bwa aln INDEX file_1.fastq > file_1.sai
+bwa aln INDEX file_2.fastq > file_2.sai
+bwa sampe INDEX file_1.sai file_2.sai file_1.fastq file_2.fastq | samtools view -q 1 > aln.sam
+
+# Extraction of same-orientation-reads (SOR) #
+
+awk '$2 ~ /113|177|65|129/ {print $2, $4, $6, $9}' aln.sam > SOR.txt
+
+# Extraction of 5-prime soft clipped reads (sCLIP) #
+
+awk '($2 ~ /147|83/ && $6 ~ /^..?S/) || ($2 ~ /99|163/ && $6 ~ /S$/) {next;} $6 ~ /^..?S/ {print $2, $4, $6, $9 }' aln.sam > sCLIP.txt
+
+# End
